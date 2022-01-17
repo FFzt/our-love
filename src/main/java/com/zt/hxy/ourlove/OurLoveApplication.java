073f08cbbcc3e0d2d36e64d96ca327e3d6557d3f
zhangtao <zhangtao@iboxpay.com>
2022-01-17 11:42:49 +0800

    添加背景图片

--- src/main/java/com/zt/hxy/ourlove/OurLoveApplication.java
+++ src/main/java/com/zt/hxy/ourlove/OurLoveApplication.java
@@ -3,6 +3,10 @@ package com.zt.hxy.ourlove;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhangtao
+ * @date 2021/12/09 16:33
+ */
 @SpringBootApplication
 public class OurLoveApplication {
 
