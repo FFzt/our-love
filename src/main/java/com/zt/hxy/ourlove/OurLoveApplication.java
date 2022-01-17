f140b61708dde2f7ea0e060e1fa1d7e397dead51
zhangtao <zhangtao@iboxpay.com>
2022-01-17 10:40:19 +0800

    init repo

---
+++ src/main/java/com/zt/hxy/ourlove/OurLoveApplication.java
@@ -0,0 +1,13 @@
+package com.zt.hxy.ourlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zt/hxy/ourlove/OurLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zt.hxy.ourlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
